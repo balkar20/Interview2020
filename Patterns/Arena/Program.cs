87a16c223608da7ef680ea8440e4ef662b5a558b
Vladislav <balkar20@yandex.ru>
2020-07-07 21:24:54 +0300

    -

---
+++ Patterns/Arena/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TickTock tt = new TickTock();
+            MyThread mt1 = new MyThread("Tick", tt);
+            MyThread mt2 = new MyThread("Tock", tt);
+            mt1.thrd.Join();
+            mt2.thrd.Join();
+
+            Console.WriteLine("Часы остановлены");
+            Console.ReadLine();
+        }
+
+        private static void Work()
+        {
+            Thread.Sleep(1000);
+        }
+    }
+
+    class TickTock
+    {
+        private object lockOn = new object();
+
+        public void Tick(bool running)
+        {
+            lock (lockOn)
+            {
+                if (!running)
+                {
+                    // Остановить часы
+                    Monitor.Pulse(lockOn);
+                    return;
+                }
+
+                Console.Write("Тик ");
+                // Разрешить выполнение метода Tock()
+                Monitor.Pulse(lockOn);
+
+                // Ожидать завершение Tock()
+                Monitor.Wait(lockOn);
+            }
+        }
+
+        public void Tock(bool running)
+        {
+            lock (lockOn)
+            {
+                if (!running)
+                {
+                    Monitor.Pulse(lockOn);
+                    return;
+                }
+
+                Console.WriteLine("так");
+                Monitor.Pulse(lockOn);
+                Monitor.Wait(lockOn);
+            }
+        }
+    }
+
+    class MyThread
+    {
+        public Thread thrd;
+        TickTock ttobj;
+
+        // Новый поток
+        public MyThread(string name, TickTock tt)
+        {
+            thrd = new Thread(this.Run);
+            ttobj = tt;
+            thrd.Name = name;
+            thrd.Start();
+        }
+
+        void Run()
+        {
+            if (thrd.Name == "Tick")
+            {
+                for (int i = 0; i < 5; i++)
+                    ttobj.Tick(true);
+                ttobj.Tick(false);
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                    ttobj.Tock(true);
+                ttobj.Tock(false);
+            }
+        }
+    }
+}
---
+++ Patterns/Arena2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Arena2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int.MaxValue is 2,147,483,647.
+            const int ConstantMax = int.MaxValue;
+            int int1;
+            int int2;
+            int variableMax = 2147483647;
+
+            // The following statements are checked by default at compile time. They do not
+            // compile.
+            //int1 = 2147483647 + 10;
+            //int1 = ConstantMax + 10;
+
+            // To enable the assignments to int1 to compile and run, place them inside 
+            // an unchecked block or expression. The following statements compile and
+            // run.
+            unchecked
+            {
+                int1 = 2147483647 + 10;
+            }
+
+            checked
+            {
+                int1 = 2147483647;
+            }
+            
+
+            Console.WriteLine($"{int1}");
+            //ITest test = new Test(2);
+            //MethTest(test);
+            //test.ShowI();
+            //Console.ReadLine();
+        }
+
+        static void MethTest(ITest test)
+        {
+            test.i = 10;
+        }
+    }
+
+    public struct Test:ITest
+    {
+        public Test(int i)
+        {
+            this.i = i;
+        }
+
+        public int i { get; set; }
+
+        public void ShowI()
+        {
+            Console.WriteLine($"Test i: {this.i}");
+        }
+    }
+
+    public interface ITest
+    {
+        int i { get; set; }
+        void ShowI();
+    }
+}
